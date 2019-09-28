5d3ddb5865d6426097e40c9e7eda5642b5d1d8c1
Arijit Patra <ari.clicks@gmail.com>
2019-09-28 23:51:57 +0530

    icons
    icons

--- src/appConstants.js
+++ src/appConstants.js
@@ -7,36 +7,43 @@ export const allTeams = [
   {
     id: 2,
     name: "Two",
-    imgSrc: "https://brandmark.io/logo-rank/random/pepsi.png"
+    imgSrc:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ56MziuMfIP_qkS2qyMdjbMwXuA0qNzIUaLWw8gNDrf4CSDjUn"
   },
   {
     id: 3,
     name: "Three",
-    imgSrc: "https://brandmark.io/logo-rank/random/pepsi.png"
+    imgSrc:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrcqEgbfHhrUyblPKps-8WBExtS4NLI1S1N7wycbMKy2OJmmZh"
   },
   {
     id: 4,
     name: "Four",
-    imgSrc: "https://brandmark.io/logo-rank/random/pepsi.png"
+    imgSrc:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOajDg92GMfv2baamYOAPcO1KjIQfPFzg7NLkolcQ3PG99xWP2"
   },
   {
     id: 5,
     name: "Five",
-    imgSrc: "https://brandmark.io/logo-rank/random/pepsi.png"
+    imgSrc:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyGwkqbnJ3Y6aEu7PhCj7U6xcgutOIre7gBqNO5BrXz1Cmj1UMgw"
   },
   {
     id: 6,
     name: "Six",
-    imgSrc: "https://brandmark.io/logo-rank/random/pepsi.png"
+    imgSrc:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnGo9CFkZz6KovuxDabq-nmNvTXQivO7JocO-0yCbXwrei4-eUmQ"
   },
   {
     id: 7,
     name: "Seven",
-    imgSrc: "https://brandmark.io/logo-rank/random/pepsi.png"
+    imgSrc:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSz38uzsXTMAqWCkaeMV7zMHRegCgNl5CaA3Qs7PqJMXAJ_0LwZ"
   },
   {
     id: 8,
     name: "Eight",
-    imgSrc: "https://brandmark.io/logo-rank/random/pepsi.png"
+    imgSrc:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1wwNFjsmptYthTZl19NLdql9XpOlmCHCsvgePJChue3OGeR_sRg"
   }
 ];
